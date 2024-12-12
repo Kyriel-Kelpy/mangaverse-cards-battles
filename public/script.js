8dafa176c35d371733f9bd42fe12f77a56de76b3
Nightraid <micheldjossou02@gmail.com>
2024-12-12 19:52:04 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -39,7 +39,7 @@ document.getElementById('whatsapp-form').addEventListener('submit', function(eve
             ).join('\n');
 
             // Appeler Twilio API pour envoyer le message WhatsApp
-            fetch('http://localhost:3000/send-card', {
+            fetch('/api/send-card', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
